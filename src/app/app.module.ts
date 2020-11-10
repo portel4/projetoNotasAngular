1dbae91b83748d52d0549ba4705b8b472899ced9
Lucas Castro <lukas.castro@live.com>
2020-11-09 21:41:51 -0300

    teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ListaComponent } from './lista/lista.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ListaComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule
